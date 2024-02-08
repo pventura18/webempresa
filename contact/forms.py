9e69020c084ee40da3933fcb2e9daabdb2eb9cfc
NEXUS\pventura <pventura@nexusgeographics.com>
2024-02-08 11:51:47 +0100

    first commit

---
+++ contact/forms.py
@@ -0,0 +1,6 @@
+from django import forms
+
+class ContactForm(forms.Form):
+    name = forms.CharField(label='Nombre', required=True, widget=forms.TextInput(attrs={'placeholder':'Escribe tu nombre', 'class':'form-control'}), min_length=3, max_length=100)
+    email = forms.EmailField(label='Email', required=True, widget=forms.EmailInput(attrs={'placeholder':'Escribe tu email', 'class':'form-control'}), min_length=3, max_length=100)
+    content = forms.CharField(label='Contenido', required=True, widget=forms.Textarea(attrs={'placeholder':'Escribe tu mensaje','rows':3, 'class':'form-control'}), min_length=10, max_length=1000)
---
+++ core/views.py
@@ -0,0 +1,15 @@
+from django.http import HttpResponse
+from django.shortcuts import render
+
+# Create your views here.
+def home(request):
+    return render(request, "core/home.html")
+
+def about(request):
+    return render(request, "core/about.html")
+
+def store(request):
+    return render(request, "core/store.html")
+
+def sample(request):
+    return render(request, "core/sample.html")
